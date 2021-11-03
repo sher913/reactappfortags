b061937b330cf69791ebb5db9f84a22b9dcdd1b9
sher913 <shermanchan@rocketmail.com>
2021-11-03 18:42:47 +0800

    Removed update button when not logged in

--- frontend/src/components/update-button.js
+++ frontend/src/components/update-button.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { useAuth0 } from "@auth0/auth0-react";
 const UpdateButton = () => {
-  return (
+  const { isAuthenticated } = useAuth0();
+  return isAuthenticated ? (
     <div className="navbar-nav me-auto">
       <button id="test" className="btn btn-primary btn-block">
+        Update Datahub GMS
+      </button>
+    </div>
+  ) : (
+    <div className="navbar-nav me-auto">
+      <button id="test" className="btn btn-primary btn-block" style={{ display: "none" }}>
         Update Datahub GMS
       </button>
     </div>
